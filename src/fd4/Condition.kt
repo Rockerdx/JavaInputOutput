9c0867c3769b19921dec124f769e345025ade8d2
Rizki <rizki.putra@stockbit.com>
2022-04-18 20:19:15 +0700

    tambah komentar, tambah contoh penggunaan when

--- src/fd4/Condition.kt
+++ src/fd4/Condition.kt
@@ -6,18 +6,34 @@ fun main() {
     println("Masukkan bilangan 1 : ")
     val bil1 = readLine()?.toIntOrNull() ?: 0
 
-
+    // ini adalah contoh condisi if dengan 2 pengecekan
     if(bil1 == 7) {
+        println("ini adalah bilangan 7")
+    } else if(bil1 == 8) {
+        println("ini adalah bilangan 8")
+    } else {
+        println("ini adalah bilangan 7")
+    }
+
+    val isSeven : Boolean = bil1 == 7
+
+    // ini contoh if dengan 1 pengecekan dan variable penampung
+    if(isSeven) {
         println("ini adalah bilangan 7")
     } else {
         println("ini bukan bilangan 7")
     }
 
-    val isSeven : Boolean = bil1 == 7
-
-    if(isSeven) {
-        println("ini adalah bilangan 7")
-    } else {
-        println("ini bukan bilangan 7")
+    // contoh when
+    when (bil1) {
+        7 -> {
+            println("ini adalah bilangan 7")
+        }
+        8 -> {
+            println("ini adalah bilangan 8")
+        }
+        else -> {
+            println("ini adalah bilangan 7")
+        }
     }
 }
